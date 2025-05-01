83b60a82c4748a217780ebc1070816e6ab11fb9c
phenriqued <pedrodias3737@gmail.com>
2025-05-01 12:09:01 -0300

    adding styling to the To Do List page.

--- src/main/java/phenriqued/ToDoList/Model/TaskEntity/TaskEntity.java
+++ src/main/java/phenriqued/ToDoList/Model/TaskEntity/TaskEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "TaskEntity")
 @Table(name = "tb_task")
@@ -17,8 +18,11 @@ public class TaskEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
     private String text;
+    @Setter
     private Boolean done;
+    @Setter
     private Boolean favorite;
 
 }
--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,6 +1,10 @@
+    //Mudança de título da pagína
     const tituloTarefaElement = document.getElementById('tituloTarefa');
 
     tituloTarefaElement.addEventListener('input', function() {
         const novoTitulo = tituloTarefaElement.textContent.trim();
         document.title = novoTitulo || 'To Do List';
     });
+
+    //adicionando evento ao clicar ou pressionar enter para adicionar uma tarefa.
+    const button = document.getElementById("plus-icon");
