044913058ac1494112e3c8badfb5f3a7c17037e2
phenriqued <pedrodias3737@gmail.com>
2025-05-06 15:23:47 -0300

    applying logging

--- src/main/java/phenriqued/ToDoList/infra/AutomaticExclude/AutomaticDeletion.java
+++ src/main/java/phenriqued/ToDoList/infra/AutomaticExclude/AutomaticDeletion.java
@@ -1,18 +1,24 @@
 package phenriqued.ToDoList.infra.AutomaticExclude;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import phenriqued.ToDoList.Model.TaskEntity.TaskEntity;
 import phenriqued.ToDoList.Repositories.TaskRepository.TaskRepository;
 
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.util.List;
 
 @Component
 @EnableScheduling
 public class AutomaticDeletion {
+
+    private static final Logger logger = LoggerFactory.getLogger(AutomaticDeletion.class);
 
     private final long HORA = 60000 * 60;
 
@@ -21,12 +27,19 @@ public class AutomaticDeletion {
 
     @Scheduled(fixedDelay = HORA)
     public void automaticTaskDeletion(){
-        repository.findAll().forEach(task -> {
-            if(LocalDateTime.now().isAfter(task.getCreateDateTime().plus(Period.ofDays(1))) ){
-                repository.delete(task);
-            }
+        logger.info("Iniciando a verificação e exclusão automática de tarefas");
 
-        });
+        List<TaskEntity> tasksToDelete = repository.findAll().stream()
+                .filter(task -> LocalDateTime.now().isAfter(task.getCreateDateTime().plus(Period.ofDays(1))))
+                .toList();
+
+        if(!tasksToDelete.isEmpty()){
+            logger.info("Encontradas {} tarefas para exclusão.", tasksToDelete.size());
+            repository.deleteAll(tasksToDelete);
+            logger.info("{} Deletas com sucesso!", tasksToDelete.size());
+        }else{
+            logger.info("Nenhuma tarefa elegível para exclusão automática no momento.");
+        }
     }
 
 }
